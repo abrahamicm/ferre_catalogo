8c76387bcad8574d4429f4342ab0567b285309db
abraham cordero <abrahamicm2@gmail.com>
2022-05-24 05:48:04 -0400

    eliminando modelos

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,29 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { ListaPreciosComponent } from './lista-precios/lista-precios.component';
 import { OfertasComponent } from './ofertas/ofertas.component';
 import { CategoriasListComponent } from './categorias-list/categorias-list.component';
-
 import { ContactosCelListComponent } from './contactos-cel-list/contactos-cel-list.component';
 import { ContactosComponent } from './contactos/contactos.component';
-
 import { IonicModule } from '@ionic/angular';
 import { RouterModule } from '@angular/router';
 import { HeaderBackComponent } from './header-back/header-back.component';
 import { FooterComponent } from './footer/footer.component';
 import { NgxDatatableModule } from '@swimlane/ngx-datatable';
-import { NgxTableBootstrapComponent } from './ngx-table-bootstrap/ngx-table-bootstrap.component';
-import { LaminasListComponent } from './laminas-list/laminas-list.component';
 import { Footer2Component } from './footer2/footer2.component';
 import { BaseMaterialListComponent } from './base-material-list/base-material-list.component';
 import { AngulosListComponent } from './angulos-list/angulos-list.component';
-
-import { OfertasListComponent } from './ofertas-list/ofertas-list.component';
-import { PletinasListComponent } from './pletinas-list/pletinas-list.component';
-import { TubosEstructuralesListComponent } from './tubos-estructurales-list/tubos-estructurales-list.component';
-import { TubosHpListComponent } from './tubos-hp-list/tubos-hp-list.component';
-import { VigasListComponent } from './vigas-list/vigas-list.component';
 import { SedesListComponent } from './sedes-list/sedes-list.component';
 
 
@@ -31,54 +20,33 @@ import { SedesListComponent } from './sedes-list/sedes-list.component';
 @NgModule({
   declarations: [HeaderComponent,
     HeaderBackComponent,
-    ListaPreciosComponent,
     OfertasComponent,
     CategoriasListComponent,
     ContactosCelListComponent,
     ContactosComponent,
     FooterComponent,
-    NgxTableBootstrapComponent,
-    LaminasListComponent,
     Footer2Component,
     BaseMaterialListComponent,
     AngulosListComponent,
-
-    OfertasListComponent,
-    PletinasListComponent,
-    TubosEstructuralesListComponent,
-    TubosHpListComponent,
-    VigasListComponent,
     SedesListComponent
-
   ],
   imports: [
     CommonModule,
     IonicModule,
     RouterModule,
     NgxDatatableModule
-
-
   ],
   exports: [
     HeaderComponent,
     HeaderBackComponent,
-    ListaPreciosComponent,
     OfertasComponent,
     CategoriasListComponent,
     ContactosCelListComponent,
     ContactosComponent,
     FooterComponent,
-    NgxTableBootstrapComponent,
-    LaminasListComponent,
     Footer2Component,
     BaseMaterialListComponent,
     AngulosListComponent,
-
-    OfertasListComponent,
-    PletinasListComponent,
-    TubosEstructuralesListComponent,
-    TubosHpListComponent,
-    VigasListComponent,
     SedesListComponent
 
 
