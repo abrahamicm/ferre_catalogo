73ac5ffdce0829d76554d08a136140052d8b9831
abraham cordero <abrahamicm2@gmail.com>
2022-06-03 13:25:07 -0400

    .

---
+++ php/tuberia_set_largo.php
@@ -0,0 +1,81 @@
+<?php
+
+function tuberia_set_largo($i, $l)
+{
+
+	update_post_meta($i, "largo", $l);
+}
+
+tuberia_set_largo(16610, 3);
+tuberia_set_largo(16611, 12.5);
+tuberia_set_largo(16612, 12.5);
+tuberia_set_largo(16613, 100);
+tuberia_set_largo(16608, 18.75);
+tuberia_set_largo(16609, 12.5);
+tuberia_set_largo(16542, 100);
+tuberia_set_largo(16558, 160);
+tuberia_set_largo(16574, 320);
+tuberia_set_largo(16590, 3);
+tuberia_set_largo(16543, 100);
+tuberia_set_largo(16559, 175);
+tuberia_set_largo(16575, 75);
+tuberia_set_largo(16591, 3);
+tuberia_set_largo(16544, 100);
+tuberia_set_largo(16560, 175);
+tuberia_set_largo(16576, 75);
+tuberia_set_largo(16592, 3);
+tuberia_set_largo(16545, 100);
+tuberia_set_largo(16561, 180);
+tuberia_set_largo(16577, 75);
+tuberia_set_largo(16593, 3);
+tuberia_set_largo(16546, 110);
+tuberia_set_largo(16562, 180);
+tuberia_set_largo(16578, 80);
+tuberia_set_largo(16594, 3);
+tuberia_set_largo(16547, 120);
+tuberia_set_largo(16563, 200);
+tuberia_set_largo(16579, 37.5);
+tuberia_set_largo(16595, 3);
+tuberia_set_largo(16548, 120);
+tuberia_set_largo(16564, 220);
+tuberia_set_largo(16580, 31.25);
+tuberia_set_largo(16596, 3);
+tuberia_set_largo(16549, 120);
+tuberia_set_largo(16565, 220);
+tuberia_set_largo(16581, 1);
+tuberia_set_largo(16597, 3);
+tuberia_set_largo(16550, 120);
+tuberia_set_largo(16566, 220);
+tuberia_set_largo(16582, 1);
+tuberia_set_largo(16598, 18.75);
+tuberia_set_largo(16551, 135);
+tuberia_set_largo(16567, 260);
+tuberia_set_largo(16583, 1);
+tuberia_set_largo(16599, 18.75);
+tuberia_set_largo(16552, 140);
+tuberia_set_largo(16568, 260);
+tuberia_set_largo(16584, 12.5);
+tuberia_set_largo(16604, 1);
+tuberia_set_largo(16537, 100);
+tuberia_set_largo(16553, 140);
+tuberia_set_largo(16569, 260);
+tuberia_set_largo(16585, 2);
+tuberia_set_largo(16605, 12.5);
+tuberia_set_largo(16538, 100);
+tuberia_set_largo(16554, 155);
+tuberia_set_largo(16570, 260);
+tuberia_set_largo(16586, 2);
+tuberia_set_largo(16606, 2);
+tuberia_set_largo(16539, 100);
+tuberia_set_largo(16555, 155);
+tuberia_set_largo(16571, 260);
+tuberia_set_largo(16587, 2);
+tuberia_set_largo(16607, 18.75);
+tuberia_set_largo(16540, 100);
+tuberia_set_largo(16556, 155);
+tuberia_set_largo(16572, 300);
+tuberia_set_largo(16588, 2);
+tuberia_set_largo(16541, 100);
+tuberia_set_largo(16557, 160);
+tuberia_set_largo(16573, 320);
+tuberia_set_largo(16589, 2);
