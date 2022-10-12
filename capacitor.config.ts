ef0d1b27d850ab6aad84f827484399371126cdc7
fyarza <fyarza@uc.edu.ve>
2022-10-11 21:15:41 -0400

    ios compilation

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'catalogo',
-  webDir: 'www',
-  bundledWebRuntime: false
+	appId: 'io.ionic.starter',
+	appName: 'catalogo',
+	webDir: 'www',
+	bundledWebRuntime: false,
+	server: { allowNavigation: ['https://ferreacerovalencia.com.ve/'] },
 };
 
 export default config;
